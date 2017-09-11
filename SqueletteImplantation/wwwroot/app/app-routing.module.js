21e4f7652f78256cb49eac1716d678e4e6209f5c
Etienne Savard <7419635@hotmail.ca>
2017-09-11 18:12:38 -0400

    Generation

---
+++ SqueletteImplantation/wwwroot/app/app-routing.module.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("@angular/router");
+var page_cat_component_1 = require("./page-cat.component");
+var index_component_1 = require("./index.component");
+exports.router = [
+    { path: '', redirectTo: '/index', pathMatch: 'full' },
+    { path: 'categorie', component: page_cat_component_1.PageCatComponent },
+    { path: 'index', component: index_component_1.IndexComponent },
+    { path: '**', component: index_component_1.IndexComponent }
+];
+exports.routes = router_1.RouterModule.forRoot(exports.router);
+//# sourceMappingURL=app-routing.module.js.map
---
+++ SqueletteImplantation/wwwroot/app/critere.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Critere = (function () {
+    function Critere(ID, Nom) {
+        this.ID = ID;
+        this.Nom = Nom;
+    }
+    return Critere;
+}());
+exports.Critere = Critere;
+//# sourceMappingURL=critere.js.map
