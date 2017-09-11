b635b332d35ce6c11e0c294b0397b4e3f9443f1c
Frédérick Picard <frederick.picard@hotmail.ca>
2017-09-11 15:37:57 -0400

    Update index.component.ts

--- SqueletteImplantation/wwwroot/app/index.component.ts
+++ SqueletteImplantation/wwwroot/app/index.component.ts
@@ -5,13 +5,9 @@ import { Component } from '@angular/core';
     templateUrl: 'app/html/index.component.html',
     styleUrls: [ 'app/css/index.component.css' ]
 })
-<<<<<<< HEAD
-export class IndexComponent 
-{ 
-=======
+
 export class IndexComponent { 
     Connexion(): void {
         console.log("test");
     }
->>>>>>> 9d69a1f5e767397c739172ebb541b2f4eed97d64
 }
