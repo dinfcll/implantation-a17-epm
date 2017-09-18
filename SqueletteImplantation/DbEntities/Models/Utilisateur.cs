7159efdb4745ea404a6d10cfcc13ee8701d17855
1544692 <allichetina@gmail.com>
2017-09-18 14:31:50 -0400

    Ajout du code pour la table Utilisateur et petit fix de css

---
+++ SqueletteImplantation/DbEntities/Models/Utilisateur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqueletteImplantation.DbEntities.Models
+{
+    public class Utilisateur
+    {
+        //Nom, Prénom, MDP, ID, Username, courriel, type user (Admin/User)
+        public int UtilId { get; set; }
+        public string UtilPren { get; set; }
+        public string UtilNom { get; set; }
+        public string UtilPWD { get; set; }
+        public string UtilUserName { get; set; }
+        public string UtilEmail { get; set; }
+        public int UtilType { get; set; }
+
+    }
+}
