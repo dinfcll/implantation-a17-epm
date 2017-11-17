78b0710137f8604901c19ae9f435ddce5dfd9c05
1544692 <allichetina@gmail.com>
2017-11-17 10:39:22 -0500

    modification du passage de l'util, arrive encore vide côté backend

--- SqueletteImplantation/wwwroot/app/utilisateur.service.ts
+++ SqueletteImplantation/wwwroot/app/utilisateur.service.ts
@@ -47,6 +47,7 @@ export class UtilisateurService {
         headers.append('Content-type', 'application/json');
         
         console.log(nouveauutilisateur);
-        return this.http.post(this.baseUrl, nouveauutilisateur);
+        return this.http.post(this.baseUrl, JSON.stringify({ "utilpren": nouveauutilisateur.utilpren, "utilnom": nouveauutilisateur.utilnom,
+            "utilusername":nouveauutilisateur.utilusername, "utilemail":nouveauutilisateur.utilemail, "utiltype":nouveauutilisateur.utiltype }), { headers });
     }
 }
